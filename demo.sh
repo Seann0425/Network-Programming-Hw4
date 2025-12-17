82b1d64ebd08f15299d678d3ce0ca1a424cc9bff
Seann0425 <seanyu0425@gmail.com>
2025-12-17 23:36:18 +0800

    Fix: add install progress

--- demo.sh
+++ demo.sh
@@ -16,7 +16,7 @@ source $VENV_DIR/bin/activate
 # 3. 安裝依賴 (如果 requirements.txt 存在)
 if [ -f "requirements.txt" ]; then
     echo "[Script] Checking dependencies..."
-    pip install -r requirements.txt > /dev/null
+    pip install -r requirements.txt
 fi
 
 # 4. 執行啟動腳本 (這裡改成你實際的啟動腳本名稱，例如 launcher.py 或 start_client.py)
